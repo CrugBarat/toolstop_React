00f014df15810b78b6839f6efdd39a840666df84
CrugBarat <craigtbarratt@gmail.com>
2020-07-16 14:58:56 +0100

    adds Top 40 image data config
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

---
+++ src/config/Top40ImageData.js
@@ -0,0 +1,42 @@
+const Top40ImageData = [
+
+  {
+    name: "bahco20",
+    url: "https://cdn11.bigcommerce.com/s-7holhynnib/product_images/uploaded_images/top-20-logos-bahco-1.png?imwidth=300"
+  },
+  {
+    name: "bosch20",
+    url: "https://cdn11.bigcommerce.com/s-7holhynnib/product_images/uploaded_images/top-20-logos-bosch-1.png?imwidth=300"
+  },
+  {
+    name: "dewalt20",
+    url: "https://cdn11.bigcommerce.com/s-7holhynnib/product_images/uploaded_images/top-20-logos-dewalt-1.png?imwidth=300"
+  },
+  {
+    name: "evolution20",
+    url: "https://cdn11.bigcommerce.com/s-7holhynnib/product_images/uploaded_images/top-20-logos-evolution-1.png?imwidth=300"
+  },
+  {
+    name: "hikoki20",
+    url: "https://cdn11.bigcommerce.com/s-7holhynnib/product_images/uploaded_images/top-20-logos-hikoki-1.png?imwidth=300"
+  },
+  {
+    name: "makita20",
+    url: "https://cdn11.bigcommerce.com/s-7holhynnib/product_images/uploaded_images/top-20-logos-makita-1.png?imwidth=300"
+  },
+  {
+    name: "metabo20",
+    url: "https://cdn11.bigcommerce.com/s-7holhynnib/product_images/uploaded_images/top-20-logos-metabo-1.png?imwidth=300"
+  },
+  {
+    name: "stanley20",
+    url: "https://cdn11.bigcommerce.com/s-7holhynnib/product_images/uploaded_images/top-20-logos-stanley-1.png?imwidth=300"
+  },
+  {
+    name: "top40",
+    url: "https://cdn11.bigcommerce.com/s-7holhynnib/product_images/uploaded_images/top-40-banner.png"
+  }
+
+]
+
+export default Top40ImageData;
