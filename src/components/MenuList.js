f2f626c070d4f7704f171503b4a4fd649ea88112
CrugBarat <craigtbarratt@gmail.com>
2020-07-16 16:51:06 +0100

    adds priority to images to enable sort
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/components/MenuList.js
+++ src/components/MenuList.js
@@ -2,7 +2,9 @@ import React, {Fragment} from 'react';
 import MenuItem from './MenuItem';
 
 const MenuList = (props) => {
-  const menuItem = props.images.map((image, index) => {
+  const menuItem = props.images.sort(function(a, b) {
+    return a.priority - b.priority;
+  }).map((image, index) => {
     return <MenuItem image={image} key={index}/>
   });
 
--- src/config/Top40ImageData.js
+++ src/config/Top40ImageData.js
@@ -2,39 +2,48 @@ const Top40ImageData = [
 
   {
     name: "bahco20",
-    url: "https://cdn11.bigcommerce.com/s-7holhynnib/product_images/uploaded_images/top-20-logos-bahco-1.png?imwidth=300"
+    url: "https://cdn11.bigcommerce.com/s-7holhynnib/product_images/uploaded_images/top-20-logos-bahco-1.png?imwidth=300",
+    priority: 6
   },
   {
     name: "bosch20",
-    url: "https://cdn11.bigcommerce.com/s-7holhynnib/product_images/uploaded_images/top-20-logos-bosch-1.png?imwidth=300"
+    url: "https://cdn11.bigcommerce.com/s-7holhynnib/product_images/uploaded_images/top-20-logos-bosch-1.png?imwidth=300",
+    priority: 5
   },
   {
     name: "dewalt20",
-    url: "https://cdn11.bigcommerce.com/s-7holhynnib/product_images/uploaded_images/top-20-logos-dewalt-1.png?imwidth=300"
+    url: "https://cdn11.bigcommerce.com/s-7holhynnib/product_images/uploaded_images/top-20-logos-dewalt-1.png?imwidth=300",
+    priority: 3
   },
   {
     name: "evolution20",
-    url: "https://cdn11.bigcommerce.com/s-7holhynnib/product_images/uploaded_images/top-20-logos-evolution-1.png?imwidth=300"
+    url: "https://cdn11.bigcommerce.com/s-7holhynnib/product_images/uploaded_images/top-20-logos-evolution-1.png?imwidth=300",
+    priority: 9
   },
   {
     name: "hikoki20",
-    url: "https://cdn11.bigcommerce.com/s-7holhynnib/product_images/uploaded_images/top-20-logos-hikoki-1.png?imwidth=300"
+    url: "https://cdn11.bigcommerce.com/s-7holhynnib/product_images/uploaded_images/top-20-logos-hikoki-1.png?imwidth=300",
+    priority: 4
   },
   {
     name: "makita20",
-    url: "https://cdn11.bigcommerce.com/s-7holhynnib/product_images/uploaded_images/top-20-logos-makita-1.png?imwidth=300"
+    url: "https://cdn11.bigcommerce.com/s-7holhynnib/product_images/uploaded_images/top-20-logos-makita-1.png?imwidth=300",
+    priority: 2
   },
   {
     name: "metabo20",
-    url: "https://cdn11.bigcommerce.com/s-7holhynnib/product_images/uploaded_images/top-20-logos-metabo-1.png?imwidth=300"
+    url: "https://cdn11.bigcommerce.com/s-7holhynnib/product_images/uploaded_images/top-20-logos-metabo-1.png?imwidth=300",
+    priority: 7
   },
   {
     name: "stanley20",
-    url: "https://cdn11.bigcommerce.com/s-7holhynnib/product_images/uploaded_images/top-20-logos-stanley-1.png?imwidth=300"
+    url: "https://cdn11.bigcommerce.com/s-7holhynnib/product_images/uploaded_images/top-20-logos-stanley-1.png?imwidth=300",
+    priority: 8
   },
   {
     name: "top40",
-    url: "https://cdn11.bigcommerce.com/s-7holhynnib/product_images/uploaded_images/top-40-banner.png"
+    url: "https://cdn11.bigcommerce.com/s-7holhynnib/product_images/uploaded_images/top-40-banner.png",
+    priority: 1
   }
 
 ]
