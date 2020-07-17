b3c9696e07bea62b4e492e9da5eae869cb22ac8c
CrugBarat <craigtbarratt@gmail.com>
2020-07-17 04:09:06 +0100

    adds search bar menu to header
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,17 @@
 import React, {Fragment} from 'react';
 import HelpBar from './helpBar/HelpBar.js';
 import Klarna from './klarna/Klarna.js';
+import SearchBar from './searchBar/SearchBar.js';
+import './Header.css';
 
 const Header = (props) => {
   return (
     <Fragment>
-      <HelpBar />
-      <Klarna />
+      <div className="header-container">
+        <HelpBar />
+        <Klarna />
+        <SearchBar />
+      </div>
     </Fragment>
   )
 }
---
+++ src/components/header/searchBar/SearchBar.js
@@ -0,0 +1,36 @@
+import React, {Fragment} from 'react';
+import signin from '../../../assets/images/signin.png';
+import basket from '../../../assets/images/basket.png';
+import './SearchBar.css';
+
+const SearchBar = (props) => {
+  return (
+    <Fragment>
+      <div className="search-bar-menu-container">
+        <div className="toolstop-logo-container">
+            <a href="/">
+              <img className="toolstop-logo" src="https://cdn11.bigcommerce.com/s-7holhynnib/images/stencil/250x100/toolstop_logo_1582119093__49512.original.png" alt="Toolstop" />
+            </a>
+        </div>
+        <div className="search-bar-container">
+          <input className="search-bar" type="text" placeholder="Search product or brand..."/>
+        </div>
+        <div className="user-container">
+        <a href="https://www.toolstop.co.uk/">
+          <div className="user-basket-container">
+            <img className="user-basket" src={basket} alt="Shopping Basket"/>
+          </div>
+        </a>
+          <a href="https://www.toolstop.co.uk/login.php">
+            <div className="sign-in-container">
+              <p className="sign-in-text">Sign in</p>
+              <img className="signin-logo" src={signin} alt="Sign in logo"/>
+            </div>
+          </a>
+        </div>
+    </div>
+    </Fragment>
+  )
+}
+
+export default SearchBar;
