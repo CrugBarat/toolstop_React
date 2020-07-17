f07c175826c363235310216d01aa757bd312fa1f
CrugBarat <craigtbarratt@gmail.com>
2020-07-17 15:23:43 +0100

    adds product menu to header
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/components/header/productMenu/ProductMenu.js
+++ src/components/header/productMenu/ProductMenu.js
@@ -4,7 +4,31 @@ import './ProductMenu.css';
 const ProductMenu = () => {
   return (
     <Fragment>
-
+      <div className="product-menu-container">
+        <ul className="product-menu">
+          <a className="product-link" href="https://www.toolstop.co.uk/power-tools/">
+            <li>POWER TOOLS</li>
+          </a>
+          <a className="product-link" href="https://www.toolstop.co.uk/hand-tools/">
+            <li>HAND TOOLS</li>
+          </a>
+          <a className="product-link" href="https://www.toolstop.co.uk/accessories/">
+            <li>ACCESSORIES</li>
+          </a>
+          <a className="product-link" href="https://www.toolstop.co.uk/equipment/">
+            <li>EQUIPMENT</li>
+          </a>
+          <a className="product-link" href="https://www.toolstop.co.uk/brands/">
+            <li>BRANDS</li>
+          </a>
+          <a className="product-link" href="https://www.toolstop.co.uk/clearance/">
+            <li>CLEARNACE</li>
+          </a>
+          <a className="product-link" href="https://www.toolstop.co.uk/special-offers/">
+            <li>SPECIAL OFFERS</li>
+          </a>
+        </ul>
+      </div>
     </Fragment>
   )
 }
