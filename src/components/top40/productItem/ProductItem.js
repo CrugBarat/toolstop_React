62453062af1cf3648061ee1a20daea547fff08cb
CrugBarat <craigtbarratt@gmail.com>
2020-07-20 00:29:37 +0100

    adds product image change on hover
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/components/top40/productItem/ProductItem.js
+++ src/components/top40/productItem/ProductItem.js
@@ -6,12 +6,24 @@ const ProductItem = ({product}) => {
 
   if(!product) return null;
 
+  function hover(e) {
+    if (product.images.length > 1) {
+      e.target.setAttribute('src', product.images[1].url_thumbnail);
+    } else {
+      e.target.setAttribute('src', product.images[0].url_thumbnail);
+    }
+  }
+
+  function unhover(e) {
+    e.target.setAttribute('src', product.images[0].url_thumbnail);
+  }
+
   return (
     <Fragment>
     <a href= {url + product.custom_url.url}>
         <div className="product-item-container">
           <div>
-            <img className="product-item-image" src={product.images[0].url_thumbnail} alt="" />
+            <img className="product-item-image" src={product.images[0].url_thumbnail} onMouseOver={hover} onMouseOut={unhover} alt="" />
           </div>
           <div>
             <p className="product-item-name">{product.name}</p>
